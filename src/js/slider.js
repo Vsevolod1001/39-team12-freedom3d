6df095f5ab9faf1a4f90bb1f03547bec657c0363
Vsevolod1001 <sevant.101@gmail.com>
2022-05-27 13:59:11 +0300

    1

---
+++ src/js/slider.js
@@ -0,0 +1,11 @@
+$('.hero__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+
+    // infinite: true,
+    // slidesToShow: 3,
+    // slidesToScroll: 3
+  });
+  
